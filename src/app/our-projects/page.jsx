0d1140f0f6e66b0de8ed4fc0c732460202bd0e34
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-08-15 05:25:48 +0600

    our project banner update

--- src/app/our-projects/page.jsx
+++ src/app/our-projects/page.jsx
@@ -1,11 +1,12 @@
 import Projects from "@/components/our-projects/main";
+import Image from "next/image";
 
 
 const OurProjectPage = () => {
-    return (
-        <div className="">
-            
-				<div
+	return (
+		<div className="">
+
+			{/* <div
 					className="h-[60vh]  bg-[white] pb-5"
 					style={{
 						backgroundImage: `url("https://i.ibb.co/s2pn0Bg/marvin-meyer-SYTO3xs06f-U-unsplash-min.jpg")`,
@@ -15,13 +16,23 @@ const OurProjectPage = () => {
 					<h1 className="text-6xl text-center text-[white] pt-32 font-bold">
 					DevsManbd Projects
 					</h1>
+				</div> */}
+			<div className="w-full h-[60vh] relative">
+				<div className="before:content-[''] w-full h-[60vh] relative before:absolute before:bg-[#0000009f] before:z-20 before:top-0 before:right-0 before:left-0 before:bottom-0">
+
+					<Image className="object-cover" fill={true} src="https://i.ibb.co/s2pn0Bg/marvin-meyer-SYTO3xs06f-U-unsplash-min.jpg" alt='' />
 				</div>
-                <div>
-                <Projects/>
-                </div>
-            </div>
-				
-    );
+
+				<div className="absolute z-30 max-w-[1240px] w-full mx-auto top-5 left-0 right-0  text-white h-full flex justify-center flex-col">
+					<h2 className='text-6xl text-center text-[white] pb-5 font-bold'>Devsmanbd Projects</h2>
+				</div>
+			</div>
+			<div>
+				<Projects />
+			</div>
+		</div>
+
+	);
 };
 
 export default OurProjectPage;
